68743822c70a6e3535520a2f0e597a1b0ed23fb5
wei <wendyhsu09@gmail.com>
2024-03-30 23:09:29 +0800

    保留已輸入的資料，只要輸入未輸入的資料

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -19,7 +19,7 @@
             <label for="title">
                 Title
             </label>
-            <input type="text" name="title" id="title">
+            <input type="text" name="title" id="title" value="{{ old('title') }}">
             @error('title')
                 <p class="error-message">{{ $message }}</p>
             @enderror
@@ -27,7 +27,7 @@
 
         <div>
             <label for="description">description</label>
-            <textarea name="description" id="description" rows="5"></textarea>
+            <textarea name="description" id="description" rows="5">{{ old('description') }}</textarea>
             @error('description')
                 <p class="error-message">{{ $message }}</p>
             @enderror
@@ -35,7 +35,7 @@
 
         <div>
             <label for="long_description">Long description</label>
-            <textarea name="long_description" id="long_description" rows="10"></textarea>
+            <textarea name="long_description" id="long_description" rows="10">{{ old('long_description') }}</textarea>
             @error('long_description')
                 <p class="error-message">{{ $message }}</p>
             @enderror
